e199feafb5d2477f65a4153b9929e4051d835fe7
Aridai Francés <afrances@intermarkit.es>
2022-05-20 07:58:35 +0200

    App theme adapted to lastfm appearance, icon changed, release variant compiled

--- app/src/main/java/com/ari/singersapp/utils/RestWsKeys.kt
+++ app/src/main/java/com/ari/singersapp/utils/RestWsKeys.kt
@@ -6,7 +6,7 @@ class RestWsKeys {
         const val API_METHOD_POST_ARTIST_SEARCH = "?method=artist.search"
         const val API_METHOD_POST_ARTIST_INFO = "?method=artist.getinfo"
         const val API_METHOD_POST_ARTIST_TOP_ALBUMS = "?method=artist.gettopalbums"
-        const val API_KEY = ""
+        const val API_KEY = "'****"
         const val API_FORMAT_RESPONSE = "json"
     }
 }
